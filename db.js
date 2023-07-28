79441e80afb40c5c7c931207c770afe5d505c497
Your Name <you@example.com>
2023-07-28 20:49:32 +0530

    mongodb atlas

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
-const mongoURL="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+// const mongoURL="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+const mongoURL="mongodb+srv://abhishek:<password>@cluster0.cwqmdti.mongodb.net/?retryWrites=true&w=majority"
+
 const connectToMongo=()=>{
     mongoose.connect(mongoURL,()=>{
         console.log('Connect to mongoose');
